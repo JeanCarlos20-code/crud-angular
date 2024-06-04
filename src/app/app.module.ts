3cec1a85fda074928f0c4ce57c2f6b428e71ea9a
JeanCarlos20-code <jeancarlos272010@gmail.com>
2024-06-04 17:59:36 -0300

    feat: implement table in page crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatTableModule } from '@angular/material/table';
-import { MatSortModule } from '@angular/material/sort';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from '../environments/environment.development';
@@ -37,6 +37,8 @@ import { CrudComponent } from './pages/crud/crud.component';
     FormsModule,
     MatIconModule,
     MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatTableModule,
     MatSortModule,
     MatPaginatorModule,
@@ -44,8 +46,8 @@ import { CrudComponent } from './pages/crud/crud.component';
   ],
   providers: [
     provideAnimationsAsync(),
-    provideFirebaseApp(() => initializeApp({ "projectId": "crud-usuarios-angular-b72aa", "appId": "1:713297389934:web:7e460731c07c82670f397f", "storageBucket": "crud-usuarios-angular-b72aa.appspot.com", "apiKey": "AIzaSyDAPXfSty5MyTNbgnswf8kiiHaMFbRDS7c", "authDomain": "crud-usuarios-angular-b72aa.firebaseapp.com", "messagingSenderId": "713297389934" })),
-    provideFirestore(() => getFirestore())
+    // provideFirebaseApp(() => initializeApp({ "projectId": "crud-usuarios-angular-b72aa", "appId": "1:713297389934:web:7e460731c07c82670f397f", "storageBucket": "crud-usuarios-angular-b72aa.appspot.com", "apiKey": "AIzaSyDAPXfSty5MyTNbgnswf8kiiHaMFbRDS7c", "authDomain": "crud-usuarios-angular-b72aa.firebaseapp.com", "messagingSenderId": "713297389934" })),
+    // provideFirestore(() => getFirestore())
   ],
   bootstrap: [AppComponent]
 })
--- src/app/pages/crud/crud.component.ts
+++ src/app/pages/crud/crud.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { User } from '../../interface/user';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-crud',
@@ -6,5 +11,46 @@ import { Component } from '@angular/core';
   styleUrl: './crud.component.scss'
 })
 export class CrudComponent {
+  displayedColumns: string[] = ['id', 'name', 'email', 'role', 'benefits', 'actions'];
+  dataSource: any;
+  listUsers: User[] = [];
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private usersService: UsersService) {
+    this.dataSource = new MatTableDataSource<any>(this.listUsers)
+  }
+  ngOnInit() {
+    this.getListUsers();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  getListUsers() {
+    this.usersService.getAllUsers().subscribe({
+      next: (response: any) => {
+        console.log("Lista de usuários firebase", response);
+        this.listUsers = response;
+
+        this.dataSource = new MatTableDataSource<any>(this.listUsers);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
